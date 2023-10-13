c223b0a459b86bb758eca2355d4e3ec4de8fa886
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-13 09:19:56 +0300

    style buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
 import './App.css'
+import logo from './images/logo.svg';
+
+const links = [
+  {text:'Product', url: "#"},
+  {text:'Company', url: "#"},
+  {text:'Connect', url: "#"}
+];
 
 function App() {
   return (
-    <>
-      Hello
-    </>
+    <div>
+
+
+      <div className='hero-wrapper'>
+        <header className='container'>
+          <img src={logo} alt='logo'/>
+          <div>
+            <nav>
+              <ul>
+                {links.map((link, idx) => {
+                  return (
+                    <li key={idx}>
+                      <a href={link.url}>{link.text}</a>
+                    </li>
+                  )
+                })}                
+              </ul>      
+            </nav>
+            <div>
+              <button className='button button-bare'>Login</button>
+              <button className='button button-primary'>Sign Up</button>
+            </div>
+          </div>
+
+        </header>
+
+        <div className='container'>
+          <h1>A modern publishing platform</h1>
+          <p>Grow your audience and build your online brand</p>
+          <div>
+            <button className='button button-primary'>Start for Free</button>
+            <button className='button button-outline'>Learn More</button>
+          </div>
+        </div>
+      
+      </div>
+
+      {/* ... */}
+    </div>
   )
 }
 
