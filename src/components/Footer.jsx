914914dde41a3756d70a3cd741f7dcabe578f68d
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-16 09:25:33 +0300

    style footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import logo from '../images/logo.svg';
+import './Footer.css'
 
 const products = [
     {text:'Overview', url: "#"},
@@ -23,38 +24,40 @@ const connections = [
 
 export default function Footer() {
     return (
-        <div>
-           <img src={logo} alt='logo'/>
-           <div>
-                <h3>Product</h3>
-                <ul>
-                    {products.map((product, productIdx) => {
-                        return (
-                            <li key={productIdx}><a href={product.url}>{product.text}</a></li>
-                        )
-                    })}
-                </ul>
+        <footer className='footer-wrapper'>
+            <div className='container footer'>
+                <img className='footer-logo' src={logo} alt='logo'/>
+                <div>
+                        <h4>Product</h4>
+                        <ul>
+                            {products.map((product, productIdx) => {
+                                return (
+                                    <li key={productIdx}><a className='footer_links' href={product.url}>{product.text}</a></li>
+                                )
+                            })}
+                        </ul>
+                </div>
+                <div>
+                    <h4>Company</h4>
+                    <ul>
+                        {companyInfo.map((company, companyIdx) => {
+                            return (
+                                <li key={companyIdx}><a className='footer_links' href={company.url}>{company.text}</a></li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div>
+                    <h4>Connect</h4>
+                    <ul>
+                        {connections.map((connect, connectIdx) => {
+                            return (
+                                <li key={connectIdx}><a className='footer_links' href={connect.url}>{connect.text}</a></li>
+                            )
+                        })}
+                    </ul>
+                </div>
            </div>
-           <div>
-                <h3>Company</h3>
-                <ul>
-                    {companyInfo.map((company, companyIdx) => {
-                        return (
-                            <li key={companyIdx}><a href={company.url}>{company.text}</a></li>
-                        )
-                    })}
-                </ul>
-           </div>
-           <div>
-                <h3>Connect</h3>
-                <ul>
-                    {connections.map((connect, connectIdx) => {
-                        return (
-                            <li key={connectIdx}><a href={connect.url}>{connect.text}</a></li>
-                        )
-                    })}
-                </ul>
-           </div>
-        </div>
+        </footer>
     )
 }
