f479bc608c4e9e1b59ba688383cae780289dd4f3
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-16 11:53:51 +0300

    finish styling SerctionThree componnet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Footer from './components/Footer';
 import Header from './components/Header';
+import SectionThree from './components/SectionThree';
 
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
   return (
     <div>
       <Header />
+
+      <SectionThree />
 
       <Footer />
     </div>
